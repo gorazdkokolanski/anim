165141c86e3ac8ee0d02c04056efe6c5a7dec8c3
gorazdkokolanski <130471310+gorazdkokolanski@users.noreply.github.com>
2024-01-05 22:21:26 +0100

    Delete e-posting-animations directory

--- e-posting-animations/src/routes/AppRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import ContactUs from "../pages/ContactUs/ContactUs";
-import LandingPage from "../pages/landingPage/landingPage";
-
-export const publicRoutes = [
-  { path: "/", element: <LandingPage /> },
-  { path: "/contact-us", element: <ContactUs /> },
-];
